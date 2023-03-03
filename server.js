77db6761455a8d6c09a5e773e418bcd43eedf092
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-03-02 19:02:13 -0800

    rendering html

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
     console.log("Here")
-    res.send('hi')
+    res.render('index', { text: "World"})
 })
 
 app.listen(3000)
