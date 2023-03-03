43d38312bbc6b11efb9aba8f9595bccdc6c627e9
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-03-03 08:05:09 -0800

    advanced rputing

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,31 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('User List')
+router.get("/", (req, res) => {
+  res.send("User List");
+});
+
+router.get("/new", (req, res) => {
+  res.send("User New Form");
+});
+
+router
+  .route("/:id")
+  .get((req, res) => {
+    console.log(req.user)
+    res.send(`Get user with id ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`Put User ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`Delete User ${req.params.id}`);
+  });
+
+const users = [{ name: "Kyle" }, { name: "Sally" }]
+router.param("id", (req, res, next, id) =>{
+    req.user = users[id]
+    next()
 })
 
-router.get('/new', (req, res) => {
-    res.send('User New Form')  
-})
-
-module.exports = router
+module.exports = router;
